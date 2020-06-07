b4a46ada82c4287252af74fb350610b757be2255
Logan Walker <loganwalker22@gmail.com>
2020-06-07 13:57:31 -0600

    made controller

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,17 @@
+class PeopleController < ApplicationController
+  def index
+    @people = Person.all
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+end
